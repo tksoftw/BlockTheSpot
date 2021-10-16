d275ef7e43eb5fc22aa9783d78730b33c0eb7505
tk <62570383+tksoftw@users.noreply.github.com>
2021-10-16 03:10:05 -0400

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -167,7 +167,7 @@ UI isn't changed.
 }
 #>
 
-$ch = Read-Host -Prompt "Optional - Remove ad placeholder and upgrade button. Press 'N' if using Spicetify (Y/N) "
+$ch = "y"
 if ($ch -eq 'y') {
     Add-Type -Assembly 'System.IO.Compression.FileSystem'
 
